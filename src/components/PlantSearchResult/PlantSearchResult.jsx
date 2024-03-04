bff55d604ea9ee69d981c771910a12503505ce3b
shirinmjr <shirin.mohajer@gmail.com>
2024-03-04 09:53:47 -0500

    added bootstrap for resposive page

--- src/components/PlantSearchResult/PlantSearchResult.jsx
+++ src/components/PlantSearchResult/PlantSearchResult.jsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
 
-export default function PlantSearchResult({plantList}) {
+export default function PlantSearchResult({ plantList }) {
 
     return (
 
         <div>
-            {plantList.length ? (
+            {plantList.length && (
                 <ul>
                     {plantList.map((p, idx) => (
                         <li key={idx}>
                             <Link to={`/${p.common_name}`}>{p.common_name}</Link>
-                        </li>    
+                        </li>
                     ))}
                 </ul>
-                ):(
-                <div>Loading</div>
-                )
+            )
             }
         </div>
     );
